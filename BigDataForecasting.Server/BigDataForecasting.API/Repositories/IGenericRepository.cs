951fbf465112f789bf26857e8857b4b45ec8527c
Semih Şanlı <semih-semih-35@hotmail.com>
2026-02-18 00:22:18 +0300

    GenericRepository eklenmesi
    GenericRepository tamamlandı

---
+++ BigDataForecasting.Server/BigDataForecasting.API/Repositories/IGenericRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq.Expressions;
+
+namespace BigDataForecasting.API.Repositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        //Read methods
+        IQueryable<T> GetAll(bool tracking = false);
+        IQueryable<T> Where(Expression<Func<T, bool>> predicate, bool tracking = false);
+        Task<T> GetByIdAsync(int id);
+        Task<bool> AnyAsync(Expression<Func<T, bool>> predicate);
+        //Write methods
+        Task AddAsync(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
